bd925d6399d36bd915dd12b20805d72df8232287
Roberto Preste <roberto.preste@gmail.com>
2020-03-02 09:09:28 +0000

    Bump version: 0.4.1 → 0.4.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("requirements_test.txt") as f:
 setup(  # pragma: no cover
     name="cc-pypackage",
     packages=[],
-    version='0.4.1',
+    version='0.4.2',
     description="My custom Cookiecutter template for a Python package.",
     install_requires=requirements, 
     tests_require=test_requirements, 
